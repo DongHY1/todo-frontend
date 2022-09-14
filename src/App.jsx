bd14684d6d75fbae803160d5bfd53fd4a1eb5cc0
haoyudong <2070343054@email.szu.edu.cn>
2022-09-14 18:07:47 +0800

    show todos in frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import InputTodo from './components/InputTodo';
+import ShowTodo from './components/ShowTodo';
 
 export default function App() {
   return (
     <div>
       <InputTodo />
+      <ShowTodo />
     </div>
   );
 }
